41b444be108b76e5f2ffd754ee03b19d57997a98
Aayushp28 <pantaayush001@gmail.com>
2026-07-25 00:27:13 +0530

    Implement user registration API

---
+++ backend/app/api/v1/auth.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.database.dependency import get_db
+from app.schemas.user import UserRegister, UserResponse
+from app.services.auth_service import register_user
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+
+@router.post(
+    "/register",
+    response_model=UserResponse,
+    status_code=201
+)
+def register(
+    user: UserRegister,
+    db: Session = Depends(get_db)
+):
+    try:
+        return register_user(db, user)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
